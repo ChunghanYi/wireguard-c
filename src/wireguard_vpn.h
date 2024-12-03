0d799b7ff5c90c8fd84480bc9214c891db2b774d
Chunghan Yi <chunghan.yi@gmail.com>
2024-12-03 12:05:34 +0900

    initial commit

--- src/wireguard_vpn.h
+++ src/wireguard_vpn.h
@@ -5,19 +5,19 @@
 extern "C" {
 #endif // __cplusplus
 
-#define WG_LOCAL_ADDRESS        IPADDR4_INIT_BYTES(10, 1, 1, 100)
-#define WG_LOCAL_NETMASK        IPADDR4_INIT_BYTES(255, 255, 255, 0)
+/**** In fact, the information below is not used. Instead, use the /etc/wireguard.conf file. ****/
 
+#define WG_LOCAL_ADDRESS        IPADDR4_INIT_BYTES(10, 1, 1, 100)  //my vpn ip address
+#define WG_LOCAL_NETMASK        IPADDR4_INIT_BYTES(255, 255, 255, 0)
 #define WG_LOCAL_NETWORK        IPADDR4_INIT_BYTES(10, 1, 1, 0)
 
 #define WG_CLIENT_PRIVATE_KEY   "oG2ZkGjJ+BcJcuK2q+MgkfmoWEPQfnzZzeWxbq1OynU="
 #define WG_CLIENT_PORT          51820
 
-//#define WG_GATEWAY_ADDRESS      IPADDR4_INIT_BYTES(10, 1, 1, 200)  // peer vpn ip address
 #define WG_PEER_ADDRESS         IPADDR4_INIT_BYTES(10, 1, 1, 200)  // peer vpn ip address
 #define WG_PEER_PUBLIC_KEY      "dZopqlLFIFCSSxIQbI1+f6sCUWlrjj4X19VC7iA34Bs="
 #define WG_PEER_PORT            51820
-#define WG_ENDPOINT_ADDRESS     IPADDR4_INIT_BYTES(192, 168, 8, 139)
+#define WG_ENDPOINT_ADDRESS     IPADDR4_INIT_BYTES(192, 168, 8, 139)  //peer real(endpoint) ip address
 
 int wireguard_setup(void);
 
